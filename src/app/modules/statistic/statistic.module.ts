0976e15a48ffb8aa6c25f41e68abde7b9713352d
Hasan Can Kose <hasancannkosee@gmail.com>
2021-02-21 05:14:41 +0300

    Add countries and world components

--- src/app/modules/statistic/statistic.module.ts
+++ src/app/modules/statistic/statistic.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CountriesComponent } from './countries/countries.component';
+import { WorldComponent } from './world/world.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CountriesComponent, WorldComponent],
   imports: [
     CommonModule
   ]
