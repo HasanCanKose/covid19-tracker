879aed39249f8848f69daacc9cd7cc65091e00a1
Hasan Can Kose <hasancannkosee@gmail.com>
2021-02-21 22:36:26 +0300

    Add td form to get input value

--- src/app/modules/layout/header/header.component.ts
+++ src/app/modules/layout/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('form', {static: true}) countryForm: NgForm;
+  countryInput;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(event: KeyboardEvent) {
+    if(event.keyCode === 13){
+      this.countryInput = this.countryForm.value.countryInput;
+      alert(this.countryInput);
+    }
+  }
 }
--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import {FormsModule} from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [HeaderComponent, FooterComponent]
 })
