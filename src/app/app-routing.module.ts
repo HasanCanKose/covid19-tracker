9314bdcfe812e40915236105e828b081066f2e02
Hasan Can Kose <hasancannkosee@gmail.com>
2021-02-22 00:12:15 +0300

    Add country routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {WorldComponent} from './modules/statistic/world/world.component';
+import {CountriesComponent} from './modules/statistic/countries/countries.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: WorldComponent}
+  {path: 'home', component: WorldComponent},
+  {path: 'countries/:country', component: CountriesComponent}
 ]
 
 @NgModule({
