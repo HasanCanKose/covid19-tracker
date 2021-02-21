7a909544a4140ab61bbaef938876ac67c9c29002
Hasan Can Kose <hasancannkosee@gmail.com>
2021-02-21 22:12:00 +0300

    Add footer template and its style

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -10,6 +10,6 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     CommonModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, FooterComponent]
 })
 export class LayoutModule { }
