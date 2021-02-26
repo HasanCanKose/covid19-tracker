da949c36c953574424abcd154511aa6373bbbf40
Hasan Can Kose <hasancannkosee@gmail.com>
2021-02-26 22:09:05 +0300

    Add chart component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatisticSummaryComponent } from './statistic-summary/statistic-summary.component';
 import { NumbersWithCommasPipe } from './pipes/numbers-with-commas.pipe';
+import { ChartComponent } from './chart/chart.component';
 
 
 
 @NgModule({
-  declarations: [StatisticSummaryComponent, NumbersWithCommasPipe],
+  declarations: [StatisticSummaryComponent, NumbersWithCommasPipe, ChartComponent],
   imports: [
     CommonModule
   ],
