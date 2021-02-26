088c474719ac379695422c575b3ec93e23cff9a8
Hasan Can Kose <hasancannkosee@gmail.com>
2021-02-27 02:05:29 +0300

    Add world data to charts

--- src/app/modules/statistic/world/world.component.ts
+++ src/app/modules/statistic/world/world.component.ts
@@ -14,6 +14,12 @@ export class WorldComponent implements OnInit {
 
   statistics: WorldResponseModel;
   dailyStatistics: WorldDailyResponseModel;
+  caseDates: string[];
+  cases: number[];
+  deathDates: string[];
+  deaths: number[];
+  recoveredDates: string[];
+  recovered: number[];
 
   constructor(private statisticService: StatisticService, private route: ActivatedRoute) {
     this.route.data.subscribe(data => this.statistics = data['statistics']);
@@ -21,6 +27,36 @@ export class WorldComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.casesObjectToArray();
+    this.deathsObjectToArray();
+    this.recoveredObjectToArray();
+  }
+
+  casesObjectToArray() {
+    this.caseDates = Object.keys(this.dailyStatistics.cases).map(key => {
+      return key
+    });
+    this.cases = Object.values(this.dailyStatistics.cases).map(value => {
+      return value
+    });
+  }
+
+  deathsObjectToArray() {
+    this.deathDates = Object.keys(this.dailyStatistics.deaths).map(key => {
+      return key
+    });
+    this.deaths = Object.values(this.dailyStatistics.deaths).map(value => {
+      return value
+    });
+  }
+
+  recoveredObjectToArray() {
+    this.recoveredDates = Object.keys(this.dailyStatistics.recovered).map(key => {
+      return key
+    });
+    this.recovered = Object.values(this.dailyStatistics.recovered).map(value => {
+      return value
+    });
   }
 
 }
