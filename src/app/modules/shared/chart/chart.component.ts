90d1057a7d16317a6a2f71db3ff3a0c66ef9fe01
Hasan Can Kose <hasancannkosee@gmail.com>
2021-02-26 22:13:09 +0300

    Add chart

--- src/app/modules/shared/chart/chart.component.ts
+++ src/app/modules/shared/chart/chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Chart} from 'node_modules/chart.js'
 
 @Component({
   selector: 'app-chart',
@@ -10,6 +11,30 @@ export class ChartComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  chart() {
+    return new Chart('my-chart', {
+      type: 'bar',
+      data: {
+        labels: [1, 2],
+        datasets: [{
+          label: 'cases',
+          data: [1, 2],
+
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
   }
 
 }
