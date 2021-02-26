d0b7081d06cc8446fd41a479d6b91d72104cbaed
Hasan Can Kose <hasancannkosee@gmail.com>
2021-02-26 22:46:19 +0300

    Add object to array methods and pass the data to static summary component

--- src/app/modules/shared/statistic-summary/statistic-summary.component.ts
+++ src/app/modules/shared/statistic-summary/statistic-summary.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {StatisticResponseModel} from '../../../models/statistic-response.model';
+import {CountryDailyResponseModel} from '../../../models/country-daily-response.model';
 
 @Component({
   selector: 'app-statistic-summary',
@@ -9,11 +10,17 @@ import {StatisticResponseModel} from '../../../models/statistic-response.model';
 export class StatisticSummaryComponent implements OnInit {
 
   @Input() statistics: StatisticResponseModel;
-
+  @Input() caseDates: string[];
+  @Input() cases: number[];
+  @Input() deathDates: string[];
+  @Input() deaths: number[];
+  @Input() recoveredDates: string[];
+  @Input() recovered: number[];
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.caseDates);
   }
 
 }
--- src/app/modules/statistic/countries/countries.component.ts
+++ src/app/modules/statistic/countries/countries.component.ts
@@ -12,6 +12,13 @@ export class CountriesComponent implements OnInit {
 
   statistics: CountryResponseModel;
   dailyStatistics: CountryDailyResponseModel;
+  caseDates: string[];
+  cases: number[];
+  deathDates: string[];
+  deaths: number[];
+  recoveredDates: string[];
+  recovered: number[];
+
 
   constructor(private route: ActivatedRoute) {
     this.route.data.subscribe(data => this.statistics = data['statistics']);
@@ -19,6 +26,35 @@ export class CountriesComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log(this.dailyStatistics.timeline);
+    this.casesObjectToArray();
+    this.deathsObjectToArray();
+    this.recoveredObjectToArray();
+  }
+
+  casesObjectToArray() {
+    this.caseDates = Object.keys(this.dailyStatistics.timeline.cases).map(key => {
+      return key
+    });
+    this.cases = Object.values(this.dailyStatistics.timeline.cases).map(value => {
+      return value
+    });
+  }
+
+  deathsObjectToArray() {
+    this.deathDates = Object.keys(this.dailyStatistics.timeline.deaths).map(key => {
+      return key
+    });
+    this.deaths = Object.values(this.dailyStatistics.timeline.deaths).map(value => {
+      return value
+    });
+  }
+
+  recoveredObjectToArray() {
+    this.recoveredDates = Object.keys(this.dailyStatistics.timeline.recovered).map(key => {
+      return key
+    });
+    this.recovered = Object.values(this.dailyStatistics.timeline.recovered).map(value => {
+      return value
+    });
   }
 }
