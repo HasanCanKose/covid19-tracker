b544782951fa25a340b81d3fcc49afda4badba48
Hasan Can Kose <hasancannkosee@gmail.com>
2021-02-26 22:07:53 +0300

    Refactor

--- src/app/modules/statistic/countries/countries.component.ts
+++ src/app/modules/statistic/countries/countries.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {CountryResponseModel} from '../../../models/country-response.model';
 import {CountryDateResponseModel} from '../../../models/country-date-response.model';
-import {StatisticService} from '../../../services/statistic.service';
 
 @Component({
   selector: 'app-countries',
