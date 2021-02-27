b4baf5441d54d3e58788e677429c13617dedc199
Hasan Can Kose <hasancannkosee@gmail.com>
2021-02-27 05:03:37 +0300

    Add ng select component to clear input

--- src/app/modules/layout/header/header.component.ts
+++ src/app/modules/layout/header/header.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {StatisticService} from '../../../services/statistic.service';
 import {CountryResponseModel} from '../../../models/country-response.model';
+import {NgSelectComponent} from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-header',
@@ -9,8 +10,13 @@ import {CountryResponseModel} from '../../../models/country-response.model';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+  @ViewChild(NgSelectComponent) ngSelectComponent: NgSelectComponent;
+
   countryName: string;
   country: [CountryResponseModel];
+  errorMessage: string = 'asdasd';
+  isError: boolean;
 
   constructor(private router: Router, private route: ActivatedRoute, private statisticService: StatisticService) { }
 
@@ -25,14 +31,16 @@ export class HeaderComponent implements OnInit {
     if(this.countryName) {
       this.statisticService.country$.next(this.countryName);
       this.router.navigate([`countries/${this.countryName}`], {relativeTo: this.route})
-        .then(result => console.log(result))
+        .then(result => this.isError = false)
         .catch(error => {
-        console.log(error);
+        this.errorMessage = error.error.message;
+        this.isError = true;
       });
     }
   }
 
   onNavigate() {
     this.router.navigate(['home']);
+    this.ngSelectComponent.handleClearClick();
   }
 }
