a273fbb77d9ec6043b2ccc79bcd5c104c191983b
Hasan Can Kose <hasancannkosee@gmail.com>
2021-02-27 04:14:45 +0300

    Fix ng select z-index problem and refactor fields

--- src/app/modules/layout/header/header.component.ts
+++ src/app/modules/layout/header/header.component.ts
@@ -4,6 +4,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {StatisticService} from '../../../services/statistic.service';
 import {map} from 'rxjs/operators';
 import {CountryResponseModel} from '../../../models/country-response.model';
+import {log} from 'util';
 
 @Component({
   selector: 'app-header',
@@ -11,24 +12,26 @@ import {CountryResponseModel} from '../../../models/country-response.model';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  countryInput: string;
-  country: [{ Country: string, Slug: string, ISO2: string }];
+  countryName: string;
+  country: [CountryResponseModel];
 
   constructor(private router: Router, private route: ActivatedRoute, private statisticService: StatisticService) { }
 
   ngOnInit(): void {
     this.statisticService.countryNames().subscribe(country => {
       this.country = country;
-
-      console.log(this.country);
   });
   }
 
 
   onSubmit() {
-      this.router.navigate([`countries/${this.countryInput}`], {relativeTo: this.route});
-
+    if(this.countryName) {
+      this.router.navigate([`countries/${this.countryName}`], {relativeTo: this.route})
+        .then(result => console.log(result))
+        .catch(error => {
+        console.log(error);
+      });
+    }
   }
 
   onNavigate() {
